7421b6a4df6258a7c375a1ef687b361e937de0fb
Thomas <thomaschou861205@gmail.com>
2018-05-26 18:03:21 +0800

    AC ver.

--- Shortest paths.cpp
+++ Shortest paths.cpp
@@ -7,10 +7,6 @@ using namespace std;
 
 int *Dijkstra(const int, const int * const *, const int, int *);
 void printPath(const int *, int, int);
-// void init_shortestParent(const int n, const int * const *weight, int source, const int dest,
-                         // const int path_limit);
-// void shortestParent(const int n, const int * const *weight, int source, const int dest,
-                    // const int path_limit, int curr_total, bool *visit, int *vertexOrder, bool *flag);
 
 int main(void) {
 	// total # of vertices
@@ -41,15 +37,12 @@ int main(void) {
 		for (int j = 0; j < n; j++) {
 			if (dist[j] != INF && j != i) {
 
-				// printf("Path(%d,%d):", i, j);
 				cout << "Path(" << i << "," << j << "):";
 				if (show_path == "Show-path") {
 					printPath(parent, i, j);
-					// init_shortestParent(n, weight, i, j, dist[j]);
 				}
 				cout << endl;
 
-				// printf("Cost:%d\n", dist[j]);
 				cout << "Cost:" << dist[j] << endl;
 			}
 		}
@@ -94,62 +87,41 @@ int *Dijkstra(const int n, const int * const *weight, const int source, int *dis
 					parent[i] = SV;
 					dist[i] = weight[SV][i] + dist[SV];
 				}
-				else if (weight[SV][i] + dist[SV] == dist[i] && SV < parent[i])
-					parent[i] = SV;
+				else if (weight[SV][i] + dist[SV] == dist[i]){
+					stack<int> parentSV;
+					stack<int> parentI;
+
+					int parentIndex = SV;
+					parentSV.push(i);
+					while (parentIndex != source){
+						parentSV.push(parentIndex);
+						parentIndex = parent[parentIndex];
+					}
+
+					parentIndex = i;
+					while (parentIndex != source){
+						parentI.push(parentIndex);
+						parentIndex = parent[parentIndex];
+					}
+
+					while (!parentSV.empty() && !parentI.empty()){
+						if (parentSV.top() < parentI.top()){
+							parent[i] = SV;
+							break;
+						}
+						else if (parentSV.top() > parentI.top()) break;
+						else {
+							parentSV.pop();
+							parentI.pop();
+						}
+					}
+				}
 			}
 		}
 	}
 
 	return parent;
 }
-
-// void init_shortestParent(const int n, const int * const *weight, int source, const int dest,
-//                          const int path_limit) {
-// // const int n, const int * const *weight, int source, const int dest, const int path_limit
-// 	bool *visit = new bool[n];
-// 	for (int i = 0; i < n; i++) visit[i] = false;
-// 	visit[source] = true;
-
-// 	int *vertexOrder = new int[n];
-// 	for (int i = 0; i < n; i++) vertexOrder[i] = -1;
-// 	vertexOrder[0] = source;
-
-// 	bool done = false;
-// 	bool *flag = &done;
-// 	shortestParent(n, weight, source, dest, path_limit, 0, visit, vertexOrder, flag);
-
-// 	int i = 0;
-// 	while (vertexOrder[i] != -1) {
-// 		cout << vertexOrder[i];
-// 		if (i != 0) cout << "<-";
-// 		i++;
-// 	}
-// }
-// void shortestParent(const int n, const int * const *weight, int source, const int dest,
-//                     const int path_limit, int curr_total, bool *visit, int *vertexOrder,
-//                     bool *flag) {
-
-// 	if (source = dest) {
-// 		*flag = true;
-// 		return;
-// 	}
-// 	for (int i = 0; i < n; i++) {
-// 		if (weight[source][i] != 0 && !visit[i] && weight[source][i] + curr_total <= path_limit) {
-// 			visit[i] = true;
-// 			*vertexOrder = i;
-// 			cout << i << "  ";
-// 			shortestParent(n, weight, i, dest, path_limit, curr_total + weight[source][i],
-// 			               visit, vertexOrder + 1, flag);
-// 			if (*flag) return;
-
-
-// 			*vertexOrder = -1;
-// 			visit[i] = false;
-// 		}
-// 	}
-// 	return;
-
-// }
 
 void printPath(const int *parent, int source, int i) {
 	if (parent[i] != i)
